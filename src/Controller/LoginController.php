06a27a08dbe52bca87d792d68c291912f7524897
Luiz “Felds” Liscia <felds@vetorzero.com.br>
2018-01-03 19:47:43 -0200

    add login template

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,7 +3,19 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-class LoginController
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/login")
+ */
+class LoginController extends Controller
 {
-    public function indexAction() {}
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('Login/index.html.twig', []);
+    }
 }
